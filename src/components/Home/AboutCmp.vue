a2bf9c9eee43741edc48901565356f90b6b8e397
hocine <hocine737718@gmail.com>
2024-02-07 09:53:33 +0100

    About Home Commit

---
+++ src/components/Home/AboutCmp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home_about">
+        <div class="about_h_title">
+            Qui sommes-nous
+        </div>
+        <p>
+            Le cabinet Global Management Services, Sarl (par abréviation GMS) a été créé en Avril 2010 ; nous comptons plus de 20 salariés et de nombreux consultants externes spécialisés dans l'administration des ressources humaines avec tous ses aspects opérationnels ainsi que le conseil en droit social (droit du travail, sécurité sociale et fiscalité salariale).<br>
+            Global Management Services possède les compétences, les ressources et le savoir-faire nécessaires pour relever vos défis en matière d’administration des ressources humaines.
+        </p>
+        <a href="/about">Voir plus</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AboutCmp'
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="home">
     <Slider/>
+    <About/>
     <Services/>
-    <SignUp/>
   </div>
 </template>
 
 <script>
 import Services from '@/components/Home/Services/ServicesCmp.vue'
 import Slider from '@/components/Home/SliderCmp.vue'
-
+import About from '@/components/Home/AboutCmp.vue'
 export default {
   name: 'HomeView',
   components: {
     Slider,
+    About,
     Services
   }
 }
