b7462065eb4ed6149b21e3c80d45b24690ac4761
hocine <hocine737718@gmail.com>
2024-02-16 19:52:14 +0100

    Revision 8 Commit

---
+++ src/views/Audit/CommitmentView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="audit">
+        <span class="audit_back">
+            <div class="audit_header">
+                Audit Gratuit
+            </div>
+            <div class="audit_step">
+                ÉTAPE 1 : Lettre d’engagement GMS
+            </div>
+            <div class="audit_graph">
+
+            </div>
+            <div class="audit_title">
+                ENGAGEMENT DE CONFIDENTIALITÉ
+            </div>
+            <form class="audit_content">
+                <p>
+                    Nous soussignés Global Management Services, Sarl, représentée par son Gérant, nous nous engageons, solidairement et individuellement, dans le cadre de la réalisation d’un audit de conformité de la gestion administrative de votre Société à ce qui suit :                
+                </p>
+                <ul>
+                    <li>A garder une discrétion totale sur tous les documents, informations, emails obtenus dans le cadre de la réalisation de l’audit de conformité notamment les documents et actes relatives à la gestion administrative du personnel aux relations de travail au sein de votre Société, contrat de travail, bulletins de paie, pratique de rémunération, mode de gestion, plan de formation, business plan et en générale tous les documents remis par les responsables habilités de votre Société, aux personnels désignés par Global Management Services ;</li>
+                    <li>A ne pas révéler, divulguer, rendre public ou utiliser pour quelque raison ou dans quelque but que ce soit, toute information ou autre élément, quel qu’il soit, qui lui a été confiée, ou pris connaissance ou obtenu dans le cadre de la réalisation de l’audit de conformité ou en toute autre occasion, se rapportant au fonctionnement et aux activités de votre Société, leurs clients, leurs fournisseurs ;</li>
+                    <li>A détruire tous les documents remis lors de la réalisation de l’audit dès la remise des conclusions de l’audit.</li>
+                </ul>
+                <p>
+                    Nous sommes tenus à ses engagements de confidentialité et de secret professionnel pour une durée indéterminée.
+                </p>
+                <div class="audit_checkbox">
+                    <label>Lu et approuvé </label>
+                    <input type="checkbox"/>
+                </div>
+            </form>
+            <div class="audit_next">
+                Suivant
+            </div>
+        </span>
+    </div>
+</template>
+<script>
+export default {
+    name:'CommitementView',
+}
+</script>
