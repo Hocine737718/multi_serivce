95a5e8589a11c5dbea22fe9bb3b52e418f7360d0
hocine <hocine737718@gmail.com>
2024-02-19 01:26:26 +0100

    Test Deploy Commit

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
+module.exports = {
+  configureWebpack: {
+    performance: {
+      hints: false // Désactive les avertissements de performance
+    }
+  }
+};
